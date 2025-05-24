a7cba733c71b326236825d0f4363cb04e6b4c8d8
malpou <malthe@grundtvigsvej.dk>
2025-05-24 17:57:44 +0200

    fix: correct version format in swagger documentation
    - Remove quotes around version in swagger.yaml to match CI generation
    - Update all swagger files to use consistent x-release-please-version format

--- docs/docs.go
+++ docs/docs.go
@@ -353,7 +353,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.0.0",
+	Version:          "0.0.0 x-release-please-version",
 	Host:             "localhost:8090",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
