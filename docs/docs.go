5ed69da664544d0c39bc8928fccd3a37b53734b4
malpou <malthe@grundtvigsvej.dk>
2025-06-08 20:50:12 +0200

    fix: update version to 1.2.0 and fix release-please configuration
    - Manually update version strings to 1.2.0 in all required files
    - Fix release-please configuration by removing glob patterns
    - The Generic updater should auto-detect x-release-please-version markers

--- docs/docs.go
+++ docs/docs.go
@@ -318,7 +318,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.1.0 x-release-please-version",
+	Version:          "1.2.0 x-release-please-version",
 	Host:             "localhost:8090",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
